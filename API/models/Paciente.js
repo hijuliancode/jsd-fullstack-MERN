032e97e1c3bfb84cce13235c58268bbd51c4bb50
JulianCode <hijuliansosa@protonmail.com>
2020-05-28 16:20:03 -0500

    Definiendo un Modelo

---
+++ API/models/Paciente.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema // Nos va a crear la estructura en la base de datos
+
+const pacienteSchema = new Schema({
+  nombre: {
+    type: String,
+    trim: true // Si alguién coloca espacios de más, antes o despues del string automaticamente los elimina
+  },
+  propietario: {
+     type: String,
+     trim: true
+  },
+  fecha: {
+    type: String,
+    trim: true
+  },
+  hora: {
+    type: String,
+    trim: true
+  },
+  sintomas: {
+    type: String,
+    trim: true
+  }
+})
+
+
+module.exports = mongoose.model('Paciente', pacienteSchema)
