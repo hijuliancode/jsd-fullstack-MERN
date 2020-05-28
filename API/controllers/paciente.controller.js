549502410760f0ad0e3c54624befb927e96a1453
JulianCode <hijuliansosa@protonmail.com>
2020-05-28 17:23:05 -0500

    Creando un Controller

---
+++ API/controllers/paciente.controller.js
@@ -0,0 +1,10 @@
+// Dentro del controlador vas a tener diferentes funcions
+// que se van a asociar en cierta forma al modelo y en cierta forma con el routing
+
+// Cuando se crea un nuevo cliente
+
+exports.nuevoCliente = (req, res, next) => {
+  // TODO: Insertar en la base de datos
+
+  res.json({ mensaje: 'El cliente se agrego correctamente' })
+}
